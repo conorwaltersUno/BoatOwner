2f22c7195e0b1f52a5b7e53945cf1025687ec52d
cwalters <cwalters@foundationmedicine.com>
2025-03-18 10:50:52 +0000

    deploy

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import http from "http";
 
 const httpsServer = http.createServer(app);
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3001;
 
 httpsServer.listen(PORT, async () => {
   console.log(`Server listening on port ${PORT}`);
